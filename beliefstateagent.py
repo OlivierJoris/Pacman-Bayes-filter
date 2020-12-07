efd15541ae67d40a23e9bbe5df76ac21b3fea793
Maxime Goffart <maximegoffart@icloud.com>
2020-12-07 10:53:44 +0100

    PEP 8

--- beliefstateagent.py
+++ beliefstateagent.py
@@ -32,9 +32,13 @@ class BeliefStateAgent(Agent):
         self.p = 0.5
         self.n = int(self.sensor_variance/(self.p*(1-self.p)))
 
-    def ghostTransition(self, mazeWidth, mazeHeight, currentPos, previousPos, pacman_position):
-      """
-        Returns the probability for a ghost to go from previousPos to currentPos.
+    def ghostTransition(
+        self, mazeWidth, mazeHeight, currentPos, previousPos, pacman_position
+    ):
+        """
+        Returns the probability for a ghost to go from previousPos
+        to currentPos.
+
         Arguments:
         ----------
         - `mazeWidth`: the width of the maze
@@ -54,47 +58,56 @@ class BeliefStateAgent(Agent):
         - The probability for a ghost to go from previousPos to currentPos
         """
 
-       # Go through a wall is not a legal action
-      if self.walls[currentPos[0]][currentPos[1]] or self.walls[previousPos[0]][previousPos[1]]:
-        return 0
+        currX = currentPos[0]
+        currY = currentPos[1]
+        prevX = previousPos[0]
+        prevY = previousPos[1]
 
-      # Stay on the same cell is not a legal action
-      if currentPos == previousPos:
-        return 0
+        # Go through a wall is not a legal action
+        if self.walls[currX][currY] or self.walls[prevX][prevY]:
+            return 0
 
-      # The ghost can only go to vertical and horizontal neighbor cells
-      if (previousPos[0] - 1 == currentPos[0] and previousPos[1] + 1 == currentPos[1]) or \
-         (previousPos[0] - 1 == currentPos[0] and previousPos[1] - 1 == currentPos[1]) or \
-         (previousPos[0] + 1 == currentPos[0] and previousPos[1] + 1 == currentPos[1]) or \
-         (previousPos[0] + 1 == currentPos[0] and previousPos[1] - 1 == currentPos[1]):
-        return 0
+        # Stay on the same cell is not a legal action
+        if currentPos == previousPos:
+            return 0
 
-      nbLegalActions = 0
+        # The ghost can only go to vertical and horizontal neighbor cells
+        if (prevX - 1 == currX and prevY + 1 == currY) \
+                or (prevX - 1 == currX and prevY - 1 == currY) \
+                or (prevX + 1 == currX and prevY + 1 == currY) \
+                or (prevX + 1 == currX and prevY - 1 == currY):
 
-      # Check if it is not a wall on the cell he can go
-      if not self.walls[previousPos[0] + 1][previousPos[1]] and previousPos[0] + 1 < mazeWidth:
-        nbLegalActions += 1
+            return 0
 
-      if not self.walls[previousPos[0] - 1][previousPos[1]] and previousPos[0] - 1 > 0:
-        nbLegalActions += 1
+        nbLegalActions = 0
 
-      if not self.walls[previousPos[0]][previousPos[1] + 1] and previousPos[1] + 1 < mazeHeight:
-        nbLegalActions += 1
+        # Check if it is not a wall on the cell he can go
+        if not self.walls[prevX + 1][prevY] and prevX + 1 < mazeWidth:
+            nbLegalActions += 1
 
-      if not self.walls[previousPos[0]][previousPos[1] - 1] and previousPos[1] - 1 > 0:
-        nbLegalActions += 1
+        if not self.walls[prevX - 1][prevY] and prevX - 1 > 0:
+            nbLegalActions += 1
 
-      gamma = 1
+        if not self.walls[prevX][prevY + 1] and prevY + 1 < mazeHeight:
+            nbLegalActions += 1
 
-      if util.manhattanDistance(currentPos, pacman_position) >= util.manhattanDistance(previousPos, pacman_position):
-        if self.ghost_type == "afraid":
-          gamma = 2
-        elif self.ghost_type == "scared":
-          gamma = 8
+        if not self.walls[prevX][prevY - 1] and prevY - 1 > 0:
+            nbLegalActions += 1
 
-      transitionProba = gamma / (gamma * nbLegalActions)
+        gamma = 1
 
-      return transitionProba
+        currDistance = util.manhattanDistance(currentPos, pacman_position)
+        prevDistance = util.manhattanDistance(previousPos, pacman_position)
+
+        if currDistance >= prevDistance:
+            if self.ghost_type == "afraid":
+                gamma = 2
+            elif self.ghost_type == "scared":
+                gamma = 8
+
+        transitionProba = gamma / (gamma * nbLegalActions)
+
+        return transitionProba
 
     def update_belief_state(self, evidences, pacman_position, ghosts_eaten):
         """
@@ -131,48 +144,71 @@ class BeliefStateAgent(Agent):
         ghostNumber, N, M = beliefStates.shape
 
         for ghost in range(ghostNumber):
-          if not ghosts_eaten[ghost]:
+            if not ghosts_eaten[ghost]:
 
-            #Initialize the belief states
-            for x in range(N):
-             for y in range(M):
-              beliefStates[ghost][x][y] = 1 / (N * M)
+                # Initialize the belief states
+                for x in range(N):
+                    for y in range(M):
+                        beliefStates[ghost][x][y] = 1 / (N * M)
 
-            sensorDistance = evidences[ghost]
+                sensorDistance = evidences[ghost]
 
-            distanceRange = [sensorDistance - sensorStandardDeviation, sensorDistance + sensorStandardDeviation]
+                distanceRange = [
+                    sensorDistance - sensorStandardDeviation,
+                    sensorDistance + sensorStandardDeviation
+                ]
 
-            normalizationFactor = 0
+                normalizationFactor = 0
 
-            for x in range(N):
-              for y in range(M):
-                distanceToPacman = math.sqrt((x - pacman_position[0]) ** 2 + (y - pacman_position[1]) ** 2)
+                for x in range(N):
+                    for y in range(M):
+                        distanceToPacman = math.sqrt(
+                            (x - pacman_position[0]) ** 2 +
+                            (y - pacman_position[1]) ** 2
+                        )
 
-                # Distance too small or too high seeing the one of the rusty sensor 
-                if distanceToPacman < distanceRange[0] or distanceToPacman > distanceRange[1]:
-                  beliefStates[ghost][x][y] = 0
+                        # Distance too small or too high seeing the one
+                        # of the rusty sensor
+                        if distanceToPacman < distanceRange[0] or\
+                                distanceToPacman > distanceRange[1]:
+                            beliefStates[ghost][x][y] = 0
 
-                else:
-                  totalProba = 0
+                        else:
+                            totalProba = 0
 
-                  for possiblePreviousX in range(x - 1, x + 2):
-                    for possiblePreviousY in range(y - 1, y + 2):
-                      # Check if we are not exiting the maze by exploring the neighbor cells
-                      if possiblePreviousX not in range(N) or possiblePreviousY not in range(M):
-                        continue
-                      else:
-                        totalProba += self.beliefGhostStates[ghost][possiblePreviousX][possiblePreviousY] * \
-                        self.ghostTransition(N, M, [x, y], [possiblePreviousX, possiblePreviousY], pacman_position)
+                            for possiblePreviousX in range(x - 1, x + 2):
+                                for possiblePreviousY in range(y - 1, y + 2):
+                                    # Check if we are not exiting the maze by
+                                    # exploring the neighbor cells
+                                    if possiblePreviousX not in range(N)\
+                                            or possiblePreviousY\
+                                            not in range(M):
+                                        continue
+                                    else:
+                                        currValue = (
+                                            self.beliefGhostStates[ghost]
+                                            [possiblePreviousX]
+                                            [possiblePreviousY]
+                                        )
+                                        update = self.ghostTransition(
+                                            N, M, [x, y],
+                                            [
+                                                possiblePreviousX,
+                                                possiblePreviousY
+                                            ],
+                                            pacman_position
+                                        )
 
+                                        totalProba += currValue * update
 
-                  beliefStates[ghost][x][y] *= totalProba
+                            beliefStates[ghost][x][y] *= totalProba
 
-                normalizationFactor += beliefStates[ghost][x][y]
+                        normalizationFactor += beliefStates[ghost][x][y]
 
-            for x in range(N):
-              for y in range(M):
-                if normalizationFactor != 0:
-                  beliefStates[ghost][x][y] /= normalizationFactor
+                for x in range(N):
+                    for y in range(M):
+                        if normalizationFactor != 0:
+                            beliefStates[ghost][x][y] /= normalizationFactor
 
         # XXX: End of your code
 
